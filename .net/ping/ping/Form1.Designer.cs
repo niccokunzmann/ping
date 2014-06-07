b68f59198e98cb8390e75c22d2a2d74890106a61
Nicco Kunzmann <niccokunzmann@googlemail.com>
2014-06-07 17:59:04 +0200

    added c# project

---
+++ .net/ping/ping/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testbutton = new System.Windows.Forms.Button();
+            this.testlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // testbutton
+            // 
+            this.testbutton.Location = new System.Drawing.Point(12, 12);
+            this.testbutton.Name = "testbutton";
+            this.testbutton.Size = new System.Drawing.Size(75, 23);
+            this.testbutton.TabIndex = 0;
+            this.testbutton.Text = "test";
+            this.testbutton.UseVisualStyleBackColor = true;
+            this.testbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // testlabel
+            // 
+            this.testlabel.AutoSize = true;
+            this.testlabel.Location = new System.Drawing.Point(93, 17);
+            this.testlabel.Name = "testlabel";
+            this.testlabel.Size = new System.Drawing.Size(16, 13);
+            this.testlabel.TabIndex = 1;
+            this.testlabel.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 270);
+            this.Controls.Add(this.testlabel);
+            this.Controls.Add(this.testbutton);
+            this.Name = "Form1";
+            this.Text = "Ping";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testbutton;
+        private System.Windows.Forms.Label testlabel;
+    }
+}
+
---
+++ .net/ping/ping/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Razorvine.Pyro;
+    
+
+namespace ping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.testlabel.Text = "testing ...";
+            string message = this.test();
+            this.testlabel.Text = message;
+        }
+
+        private String test()
+        {
+            using (NameServerProxy ns = NameServerProxy.locateNS(null))
+            {
+                using (PyroProxy something = new PyroProxy(ns.lookup("ping.playfield")))
+                {
+                    object result = something.call("test", 42, "hello", new int[] { 1, 2, 3 });
+                    string message = (string)result;  // cast to the type that 'pythonmethod' returns
+                    return message;
+                }
+            }
+
+        }
+    }
+}
